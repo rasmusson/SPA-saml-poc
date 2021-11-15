1c4f7ec102d3315016c766f3c69a6aa3b13afa7a
Stefan Rasmusson <rasmusson.stefan@gmail.com>
2021-11-15 21:30:02 +0100

    changing nameid from saml 1 to 2

--- scripts/configure-adfs-keycloak-relying-party.ps1
+++ scripts/configure-adfs-keycloak-relying-party.ps1
@@ -14,8 +14,5 @@ c:[Type == "http://schemas.microsoft.com/ws/2008/06/identity/claims/windowsaccou
 
 @RuleTemplate = "MapClaims"
 @RuleName = "Email ad nameid"
-c:[Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress"] => issue(Type ="http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier", Issuer = c.Issuer, OriginalIssuer = c.OriginalIssuer, Value = c.Value, ValueType = c.ValueType, Properties["http://schemas.xmlsoap.org/ws/2005/05/identity/claimproperties/format"] = "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress");
+c:[Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress"] => issue(Type ="http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier", Issuer = c.Issuer, OriginalIssuer = c.OriginalIssuer, Value = c.Value, ValueType = c.ValueType, Properties["http://schemas.xmlsoap.org/ws/2005/05/identity/claimproperties/format"] = "urn:oasis:names:tc:SAML:2.0:nameid-format:email");
 '@
-
-
-Get-AdfsRelyingPartyTrust -Name 'keycloak'
