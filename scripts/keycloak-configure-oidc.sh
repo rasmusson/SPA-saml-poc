433c576c2de5a3f15f3498973f9be61364bfb98c
Stefan Rasmusson <rasmusson.stefan@gmail.com>
2024-09-01 10:54:21 +0200

    Pretty print for readbility

--- scripts/keycloak-configure-oidc.sh
+++ scripts/keycloak-configure-oidc.sh
@@ -8,4 +8,14 @@ kcadm.sh create users -r oidcrealm -s username=test -s enabled=true -o --fields 
 kcadm.sh set-password -r oidcrealm --username test --new-password test
 
 kcadm.sh create clients -r oidcrealm -f - << EOF
-{"clientId" : "oidc-client", "redirectUris": ["http://react:3000"], "webOrigins": ["+"], "standardFlowEnabled": true, "publicClient": true, "attributes": {"pkce.code.challenge.method":"S256"}}
+  {
+    "clientId" : "oidc-client",
+    "redirectUris": ["http://react:3000"],
+    "webOrigins": ["+"],
+    "standardFlowEnabled": true,
+    "publicClient": true,
+    "attributes":
+      {
+        "pkce.code.challenge.method":"S256"
+      }
+  }
